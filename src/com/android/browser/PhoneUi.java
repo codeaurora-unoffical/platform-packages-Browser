d0f1ecd4a5b88cdcab07b800564b3eb67a3769b2
sunhailong <sunhailong@y4.com>
2013-04-10 09:41:26 +0800

    bufix:has not show Browser Page Info gate

--- src/com/android/browser/PhoneUi.java
+++ src/com/android/browser/PhoneUi.java
@@ -181,7 +181,7 @@ public class PhoneUi extends BaseUi {
         }
         MenuItem info = menu.findItem(R.id.page_info_menu_id);
         if (info != null) {
-            info.setVisible(false);
+            info.setVisible(true);
         }
         MenuItem newtab = menu.findItem(R.id.new_tab_menu_id);
         if (newtab != null && !mUseQuickControls) {
